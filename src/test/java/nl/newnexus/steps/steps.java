1eac32416e0fb20f89afac1bbd94fa7f6dc084aa
TEST-PC\Tester <robertvanbuiten@gmail.com>
2017-04-17 22:22:28 +0200

    Aanmaken van een account via het scherm geautomatiseerd

--- src/test/java/nl/newnexus/steps/steps.java
+++ src/test/java/nl/newnexus/steps/steps.java
@@ -1,38 +1,112 @@
 package nl.newnexus.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.nl.Als;
 import cucumber.api.java.nl.Dan;
 import cucumber.api.java.nl.En;
 import cucumber.api.java.nl.Gegeven;
+import nl.newnexus.pages.CreateAccount;
 import org.junit.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Tester on 4/17/2017.
  */
 public class steps {
-    @Gegeven("^het scherm ‘account aanmaken’ zichtbaar is$")
-    public void hetSchermAccountAanmakenZichtbaarIs() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        Assert.assertEquals("Deze som klopt niet",1+1,2);
+
+    protected static WebDriver driver;
+    private static String browserType = "chrome";
+
+    private String emailadres = "";
+
+    @Before
+    public void Start() {
+
+        if (driver == null  ) {
+            System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().timeouts()     .implicitlyWait(5, TimeUnit.SECONDS)
+                    .pageLoadTimeout(30, TimeUnit.SECONDS)
+                    .setScriptTimeout(30, TimeUnit.SECONDS);
+            driver.manage().deleteAllCookies();
+        }
     }
 
-    @Als("^ik een accountgegevens invul voor \"([^\"]*)\" \"([^\"]*)\", \"([^\"]*)\" en \"([^\"]*)\" met een standaard adres$")
-    public void ikEenAccountgegevensInvulVoorEnMetEenStandaardAdres(String arg0, String arg1, String arg2, String arg3) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @After
+    public void Stop(Scenario scenario) {
+
+        if (driver != null) {
+            try {
+                if (scenario.isFailed()) {
+                    final byte[] screenshot = ((TakesScreenshot) driver)
+                            .getScreenshotAs(OutputType.BYTES);
+                    scenario.embed(screenshot, "image/png");
+                }
+            } finally {
+                driver.quit();
+            }
+        }
+
+        driver = null;
     }
+
+
+    @Als("^ik een accountgegevens invul voor \"([^\"]*)\" \"([^\"]*)\", \"([^\"]*)\" en \"([^\"]*)\" met een standaard adres$")
+    public void ikEenAccountgegevensInvulVoorEnMetEenStandaardAdres(String voornaam, String achternaam, String geboortedatum, String emailadres) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        CreateAccount create = new CreateAccount(driver);
+        Assert.assertEquals("",true,create.wordtPaginaGetoond());
+
+        Random rd = new Random();
+        if (emailadres.equalsIgnoreCase("#random")) {
+            emailadres = rd.nextInt(1000000)+ "test@test.nl";
+        }
+        //
+
+        Assert.assertEquals("",true,create.vulAccountInformatieIn(voornaam,achternaam,geboortedatum,emailadres));
+        this.emailadres = emailadres;
+    }
+
+    @Gegeven("^het scherm ‘account aanmaken’ zichtbaar is$")
+    public void hetSchermAccountAanmakenZichtbaarIs() throws Throwable {
+
+        // Write code here that turns the phrase above into concrete actions
+        this.driver.get("http://localhost/catalog");
+        this.driver.manage().window().maximize();
+
+        String title = this.driver.getTitle();
+        Assert.assertTrue(title.toLowerCase().contains("newnexus movies and more"));
+
+        WebElement element = this.driver.findElement(By.linkText("create an account"));
+        if (element.isDisplayed())
+            element.click();
+
+        CreateAccount create = new CreateAccount(this.driver);
+        Assert.assertEquals("",true,create.wordtPaginaGetoond());
+
+        //Assert.assertEquals("Kan niet op de link klikken", true, main.GoToCreateAnAccount());
+    }
+
 
     @En("^het password met \"([^\"]*)\"$")
     public void hetPasswordMet(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        CreateAccount create = new CreateAccount(driver);
+        Assert.assertEquals("", true, create.vulPasswordIn(arg0, arg0));
     }
 
     @En("^als ik op de knop ‘aanmaken' klik$")
     public void alsIkOpDeKnopAanmakenKlik() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        CreateAccount create = new CreateAccount(driver);
+        Assert.assertEquals("",true,create.clickOpAanmaken());
     }
 
     @Dan("^komen mijn gegevens in de database$")
@@ -46,4 +120,6 @@ public class steps {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
+
+
 }
--- src/test/resources/features/aanmaken account.feature
+++ src/test/resources/features/aanmaken account.feature
@@ -8,8 +8,8 @@ Functionaliteit: Account aanmaken op de webshop
   Gegeven het scherm ‘account aanmaken’ zichtbaar is
 
   Als ik een accountgegevens invul voor "John" "tester", "08/24/1978" en "#random" met een standaard adres
-  En het password met "test125"
-  En als ik op de knop ‘aanmaken' klik
+    En het password met "test125"
+    En als ik op de knop ‘aanmaken' klik
 
   Dan komen mijn gegevens in de database
-  En kan ik inloggen
+    En kan ik inloggen

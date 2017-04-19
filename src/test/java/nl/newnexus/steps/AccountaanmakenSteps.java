f8cb74a7caf38d00d767bc670ed5e9050e1d8351
Carl <carlterbeek@home.nl>
2017-04-19 18:20:18 +0200

    Steps inloggen aangepast

--- src/test/java/nl/newnexus/steps/AccountaanmakenSteps.java
+++ src/test/java/nl/newnexus/steps/AccountaanmakenSteps.java
@@ -5,6 +5,11 @@ import cucumber.api.java.nl.Als;
 import cucumber.api.java.nl.Dan;
 import cucumber.api.java.nl.Gegeven;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Created by Tester on 4/19/2017.
  */
@@ -12,18 +17,31 @@ public class AccountaanmakenSteps {
     @Gegeven("^Gebruiker navigeert naar website$")
     public void gebruikerNavigeertNaarWebsite() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        try {
+            URL myURL = new URL("http://localhost/catalog/");
+            URLConnection myURLConnection = myURL.openConnection();
+            myURLConnection.connect();
+        }
+        catch (MalformedURLException e) {
+            // new URL() failed
+            // ...
+        }
+        catch (IOException e) {
+            // openConnection() failed
+            // ...
+        }
+
     }
 
     @Als("^gebruiker zijn gegevens invult$")
     public void gebruikerZijnGegevensInvult() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
     }
 
     @Dan("^word er een nieuwe account voor gebruiker aangemaakt$")
     public void wordErEenNieuweAccountVoorGebruikerAangemaakt() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
     }
 }
